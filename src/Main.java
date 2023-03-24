46df72e4c5895f06f2a344100312ceee3fac73d1
Admin <admin@MacBook-Air-Admin.local>
2023-03-24 22:15:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        method( "java");
+    }
+
+    static void method(String... i) {
+        for (String k : i) {
+            System.out.println("мен сени жакшы кором "+k );
+        }
+
+    }
+
+}
+
+
+
+
+
