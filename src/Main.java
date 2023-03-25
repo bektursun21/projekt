e14b3e9b8b7c3976135c17d2446a01f7f9175c7a
Admin <admin@MacBook-Air-Admin.local>
2023-03-25 21:41:55 +0300

    first comint

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        method( "java");
-    }
 
-    static void method(String... i) {
-        for (String k : i) {
-            System.out.println("мен сени жакшы кором "+k );
+        String i = "  i like java!!!";
+        System.out.println(i.charAt(i.length()-1));
+        System.out.println(i.endsWith("!!!"));
+        System.out.println(i.startsWith("i like"));
+        System.out.println(i.contains("java"));
+        System.out.println(i.replace("a","o"));
+        System.out.println(i.toUpperCase());
+        System.out.println(i.toLowerCase());
+        System.out.println(i.substring(9,13).toUpperCase());
+        System.out.println(i.toLowerCase());
+
         }
 
-    }
 
-}
+        }
+
+
+
+
 
 
 
